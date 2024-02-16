209ea6adbc9f517e2aac9848cae5957f4a5fd2f3
TobiDeBoumaa <SHITHUB@irion.io>
2024-02-16 16:58:17 +0100

    added tabs

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # raylib-template
 
+![Alt text](image.png)
+
 Because of the submodules, this repo needs to be cloned using:
 ```
 git clone --recursive git@github.com:TobiDeBoumaa/raylib-template.git
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <iostream>
 #include "raylib.h"
 #define RAYGUI_IMPLEMENTATION
 #define RAYGUI_SUPPORT_ICONS
 #include "raygui.h"
-#include "dark/style_dark.h"
+// #include "dark/style_dark.h"
 
-#include <string> 
+#include <string>
 
 #define MAX_COLUMNS 20
 
@@ -21,9 +22,13 @@ int main()
     int screenWidth = 1000;
     int screenHeight = 600;
 
+    const char *tabLabels[] = {"Controls", "3D-Test", "Tab 3"};
+    int tabCount = sizeof(tabLabels) / sizeof(tabLabels[0]);
+    int selectedTab = 0;
+
     SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_ALWAYS_RUN | FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
     InitWindow(screenWidth, screenHeight, "Example Window");
-    GuiLoadStyleDark();
+    // GuiLoadStyleDark();
 
     // layout_name: controls initialization
     //----------------------------------------------------------------------------------
@@ -36,77 +41,88 @@ int main()
 
     // SetTargetFPS(120);
     //--------------------------------------------------------------------------------------
+    Camera camera = {0};
+            camera.position = Vector3{4.0f, 3.0f, 4.0f};
+            camera.target = Vector3{0.0f, 1.8f, 0.0f};
+            camera.up = Vector3{0.0f, 1.0f, 0.0f};
+            camera.fovy = 60.0f;
+            camera.projection = CAMERA_ORTHOGRAPHIC;
 
+            // Generates some random columns
+            float heights[MAX_COLUMNS] = {0.0f};
+            Vector3 positions[MAX_COLUMNS] = {0};
+            Color colors[MAX_COLUMNS] = {0};
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Implement required update logic
-        //----------------------------------------------------------------------------------
-        Camera camera = {0};
-        camera.position = Vector3{4.0f, 3.0f, 4.0f};
-        camera.target = Vector3{0.0f, 1.8f, 0.0f};
-        camera.up = Vector3{0.0f, 1.0f, 0.0f};
-        camera.fovy = 60.0f;
-        camera.projection = CAMERA_ORTHOGRAPHIC;
 
-        // Generates some random columns
-        float heights[MAX_COLUMNS] = {0.0f};
-        Vector3 positions[MAX_COLUMNS] = {0};
-        Color colors[MAX_COLUMNS] = {0};
-
-        for (int i = 0; i < MAX_COLUMNS; i++)
-        {
-            heights[i] = static_cast<float>(GetRandomValue(1, 12));
-            positions[i] = Vector3{static_cast<float>(GetRandomValue(-15, 15)), heights[i] / 2, static_cast<float>(GetRandomValue(-15, 15))};
-            colors[i] = Color{static_cast<unsigned char>(GetRandomValue(20, 255)), static_cast<unsigned char>(GetRandomValue(10, 55)), 30, 255};
-        }
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
-
-        BeginMode3D(camera);
-
-        DrawPlane(Vector3{0.0f, 0.0f, 0.0f}, Vector2{32.0f, 32.0f}, LIGHTGRAY); // Draw ground
-        DrawCube(Vector3{-16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, BLUE);         // Draw a blue wall
-        DrawCube(Vector3{16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, LIME);          // Draw a green wall
-        DrawCube(Vector3{0.0f, 2.5f, 16.0f}, 32.0f, 5.0f, 1.0f, GOLD);          // Draw a yellow wall
-
-        // Draw some cubes around
-        for (int i = 0; i < MAX_COLUMNS; i++)
+        int selectedTab2 = GuiTabBar(Rectangle{0, 0, float(GetScreenWidth()), 20}, tabLabels, tabCount, &selectedTab);
+        switch (selectedTab)
         {
-            DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
-            DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
-        }
+        case 0:
+            // Draw content for Tab 1
+            if (DropdownBox002EditMode)
+                GuiLock();
 
-        EndMode3D();
+            if (GuiButton(Rectangle{float(GetScreenWidth() / 2) - 60, float(GetScreenHeight() / 2) - 12, 120, 24}, "A BUTTON"))
+                Button001(WindowBox000Active);
+
+            if (WindowBox000Active)
+            {
+                WindowBox000Active = !GuiWindowBox(Rectangle{280, 120, 480, 344}, "#198# SAMPLE TEXT");
+
+                GuiStatusBar(Rectangle{280, 440, 120, 24}, "SAMPLE TEXT");
+                GuiListView(Rectangle{552, 280, 120, 72}, "ONE;TWO;THREE", &ListView004ScrollIndex, &ListView004Active);
+                if (GuiDropdownBox(Rectangle{328, 272, 120, 24}, "ONE;TWO;THREE", &DropdownBox002Active, DropdownBox002EditMode))
+                    DropdownBox002EditMode = !DropdownBox002EditMode;
+            }
+
+            GuiUnlock();
+            break;
+        case 1:
+            // Draw content for Tab 2
+            
+
+            for (int i = 0; i < MAX_COLUMNS; i++)
+            {
+                heights[i] = static_cast<float>(GetRandomValue(1, 12));
+                positions[i] = Vector3{static_cast<float>(GetRandomValue(-15, 15)), heights[i] / 2, static_cast<float>(GetRandomValue(-15, 15))};
+                colors[i] = Color{static_cast<unsigned char>(GetRandomValue(20, 255)), static_cast<unsigned char>(GetRandomValue(10, 55)), 30, 255};
+            }
+            BeginMode3D(camera);
+
+            DrawPlane(Vector3{0.0f, 0.0f, 0.0f}, Vector2{32.0f, 32.0f}, LIGHTGRAY); // Draw ground
+            DrawCube(Vector3{-16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, BLUE);         // Draw a blue wall
+            DrawCube(Vector3{16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, LIME);          // Draw a green wall
+            DrawCube(Vector3{0.0f, 2.5f, 16.0f}, 32.0f, 5.0f, 1.0f, GOLD);          // Draw a yellow wall
+
+            // Draw some cubes around
+            for (int i = 0; i < MAX_COLUMNS; i++)
+            {
+                DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
+                DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+            }
+
+            EndMode3D();
+            break;
+        case 2:
+            // Draw content for Tab 3
+            if(selectedTab2 != -1)
+                std::cout << selectedTab2 << std::endl;
+                tabCount--;
+            break;
+        }
 
         // raygui: controls drawing
         //----------------------------------------------------------------------------------
-        if (DropdownBox002EditMode)
-            GuiLock();
 
-        if (GuiButton(Rectangle{float(GetScreenWidth() / 2) - 60, float(GetScreenHeight() / 2) - 12, 120, 24}, "A BUTTON"))
-            Button001(WindowBox000Active);
-
-        if (WindowBox000Active)
-        {
-            WindowBox000Active = !GuiWindowBox(Rectangle{280, 120, 480, 344}, "#198# SAMPLE TEXT");
-
-            GuiStatusBar(Rectangle{280, 440, 120, 24}, "SAMPLE TEXT");
-            GuiListView(Rectangle{552, 280, 120, 72}, "ONE;TWO;THREE", &ListView004ScrollIndex, &ListView004Active);
-            if (GuiDropdownBox(Rectangle{328, 272, 120, 24}, "ONE;TWO;THREE", &DropdownBox002Active, DropdownBox002EditMode))
-                DropdownBox002EditMode = !DropdownBox002EditMode;
-        }
-
-        GuiUnlock();
         //----------------------------------------------------------------------------------
         std::string fpsText = "FPS: " + std::to_string(GetFPS());
         GuiStatusBar(Rectangle{0, (float)GetScreenHeight() - 20, (float)GetScreenWidth(), 20}, fpsText.c_str());
-        //DrawFPS(10, 10);
+        // DrawFPS(10, 10);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
